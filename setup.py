4c6fada7a340b113e942be364fc668c2f3361b1a
Tim Willis <tim.willis2@coxinc.com>
2015-06-08 16:58:16 -0500

    Add code.

---
+++ setup.py
@@ -0,0 +1,10 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='urllib2_openers',
+    version="0.0.1",
+    py_modules=['urllib2_openers'],
+)
